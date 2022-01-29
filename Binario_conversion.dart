de722963847dd978d0567ff8de11fcbd5d6cf479
Eduardo-Aldana <91927634+Eduardo-Aldana@users.noreply.github.com>
2022-01-29 17:38:24 -0600

    Update Binario_conversion.dart

--- Binario_conversion.dart
+++ Binario_conversion.dart
@@ -1,3 +1,14 @@
+/* Nombre de la escuela: Universidad Tecnologica Metropolitana
+    Asignatura: Desarrollo Movil Multiplataforma
+    Nombre del Maestro: Chuc Uc Joel Ivan
+    Nombre de la actividad: Ejercicio2
+    Nombre del alumno: Aldana Chuc Eduardo Aldana
+    Cuatrimestre: 5
+    Grupo: A
+    Parcial: 1
+    */
+
+
 import 'dart:io';
 
 void main() {
